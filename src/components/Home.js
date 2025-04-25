09063aca10252110ddc55287e889e9f9f33a3710
ibrahim <your-email@example.com>
2025-04-25 09:22:33 +0300

    react hooks-hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Welcome, {name}!</h1>
+      <p>Based in {city}</p>
+    </div>
+  );
 }
 
 export default Home;
